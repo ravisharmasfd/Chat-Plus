504f524006db1676b8582396ad7ba0afa64d2eb0
RAVI SHARMA <118259227+ravisharmasfd@users.noreply.github.com>
2023-05-01 17:38:13 +0530

    emoji added

--- Frontend/src/Components/AddMessage.tsx
+++ Frontend/src/Components/AddMessage.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { StateType, UserType } from "../Types";
 import { TailSpin } from "react-loader-spinner";
+import InputEmoji from "react-input-emoji";
 interface Props {
   addMessage: AddMessageFn;
 }
@@ -14,10 +15,7 @@ function AddMessage({ addMessage }: Props) {
   const user = useSelector<StateType>((state) => state.auth.user) as UserType;
   const [text, setText] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
-  return (
-    <div className="bottom-5 bg-purple-100 p-4 w-full rounded-xl">
-      <form
-        onSubmit={async (e) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
          try {
           e.preventDefault();
           setLoading(true);
@@ -27,11 +25,15 @@ function AddMessage({ addMessage }: Props) {
          } catch (error) {
           
          }
-        }}
+        }
+  return (
+    <div className="bottom-5 bg-purple-100 p-4 w-full rounded-xl">
+      <form
+        onSubmit={handleSubmit}
         className="flex justify-between items-center p-2"
       >
         <div className="flex-1">
-          <input
+          {/* <input
             type="text"
             value={text}
             onChange={(e) => {
@@ -39,12 +41,16 @@ function AddMessage({ addMessage }: Props) {
             }}
             placeholder="New message here..."
             className="w-full rounded-full p-2 focus:outline-none"
-          />
+          /> */}
+          <InputEmoji
+      value={text}
+      onChange={setText}
+      cleanOnEnter
+      onEnter={handleSubmit}
+      placeholder="Type a message"
+    />
         </div>
         <div className="flex items-center space-x-2">
-          <button type="button" className="focus:outline-none">
-            emoji
-          </button>
           {loading ? (
             <TailSpin
               height="80"
